983d8b796948249833d20a83e296b42a341682c9
Henok Biadglign Ademtew <46082799+HenokB@users.noreply.github.com>
2022-02-21 22:08:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:bmi_calculator/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -30,9 +29,7 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.black,
         )
-      //HomeScreen(),
     );
   }
-
 }
 
